2722f069fc72d4479a0cdc9abfe041cf37a174e4
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-04 14:00:54 +0100

    add a leader function to highlight who is leading

--- index.js
+++ index.js
@@ -12,31 +12,46 @@ function addOne(parameter) {
     currentGuestScore += 1;
     guest.textContent = currentGuestScore;
   }
+  leader();
 }
 
 function addTwo(parameter) {
-    if (parameter === "two") {
-        currentHomeScore += 2;
-        home.textContent = currentHomeScore;
-      } else {
-        currentGuestScore += 2;
-        guest.textContent = currentGuestScore;
-      }
+  if (parameter === "two") {
+    currentHomeScore += 2;
+    home.textContent = currentHomeScore;
+  } else {
+    currentGuestScore += 2;
+    guest.textContent = currentGuestScore;
+  }
+  leader();
 }
 
 function addThree(parameter) {
-    if (parameter === "three") {
-        currentHomeScore += 3;
-        home.textContent = currentHomeScore;
-      } else {
-        currentGuestScore += 3;
-        guest.textContent = currentGuestScore;
-      }
+  if (parameter === "three") {
+    currentHomeScore += 3;
+    home.textContent = currentHomeScore;
+  } else {
+    currentGuestScore += 3;
+    guest.textContent = currentGuestScore;
+  }
+  leader();
 }
 
-function reset(){
-    guest.textContent = 0;
-    home.textContent = 0;
-    currentHomeScore = 0;
-    currentGuestScore = 0;
+function leader() {
+  if (currentHomeScore > currentGuestScore) {
+    home.classList.add("leader");
+    guest.classList.remove("leader");
+  } else if (currentHomeScore < currentGuestScore) {
+    guest.classList.add("leader");
+    home.classList.remove("leader");
+  }
+}
+
+function reset() {
+  guest.textContent = 0;
+  home.textContent = 0;
+  currentHomeScore = 0;
+  currentGuestScore = 0;
+  home.classList.remove("leader");
+  guest.classList.remove("leader");
 }
