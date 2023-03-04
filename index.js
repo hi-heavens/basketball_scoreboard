e0dbc59997a437b5d6d9ef36bfa11d8a961eaf78
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-03-04 07:08:19 +0100

    complete the functions to increase the score

---
+++ index.js
@@ -0,0 +1,35 @@
+let home = document.getElementById("home");
+let guest = document.getElementById("guest");
+
+let currentHomeScore = parseInt(home.innerText);
+let currentGuestScore = parseInt(guest.innerText);
+
+function addOne(parameter) {
+  if (parameter === "one") {
+    currentHomeScore += 1;
+    home.textContent = currentHomeScore;
+  } else {
+    currentGuestScore += 1;
+    guest.textContent = currentGuestScore;
+  }
+}
+
+function addTwo(parameter) {
+    if (parameter === "two") {
+        currentHomeScore += 2;
+        home.textContent = currentHomeScore;
+      } else {
+        currentGuestScore += 2;
+        guest.textContent = currentGuestScore;
+      }
+}
+
+function addThree(parameter) {
+    if (parameter === "three") {
+        currentHomeScore += 3;
+        home.textContent = currentHomeScore;
+      } else {
+        currentGuestScore += 3;
+        guest.textContent = currentGuestScore;
+      }
+}
